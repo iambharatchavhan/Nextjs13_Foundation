f3650f10e8dd0693e052cb3107b123648adef56e
bharat chavhan <bharatchavhan141@gmail.com>
2023-12-14 19:17:40 +0530

    first commit

---
+++ my-nextjs-project/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+
+  console.log("thats how its shown on console its also visible on terminal")
+
+  return (
+    <main>
+      <h1>Hello world</h1>
+    </main>
+  )
+}
